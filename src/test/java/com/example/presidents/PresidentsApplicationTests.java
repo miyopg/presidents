0eb3d32f134dd6973e13b5c0498b2b3c569df37a
miyopg <miyo001@gmail.com>
2022-08-27 14:05:56 +0200

    initial commit

---
+++ src/test/java/com/example/presidents/PresidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
